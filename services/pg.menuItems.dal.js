512f42e9b1eaff7c3fc01d3e17cacf00038716c5
Corina Jewer <corina.jewer@keyin.com>
2024-03-20 15:50:51 -0230

    Minor Code Refractoring

--- services/pg.menuItems.dal.js
+++ services/pg.menuItems.dal.js
@@ -7,7 +7,6 @@ var getMenuItems = function() {
     ORDER BY menu_id ASC;`
     dal.query(sql, [], (err, result) => {
       if (err) {
-        // Logging would notmally be placed here.
         if(DEBUG) console.log(err);
         reject(err);
       } else {
@@ -23,7 +22,6 @@ var getMenuItemById = function(menu_id) {
     const sql = `SELECT  menu_id, name, description, price, category, image_url FROM public."Menu_Items" WHERE menu_id = $1`;
     dal.query(sql, [menu_id], (err, result) => {
       if (err) {
-        // Logging would normally be placed here.
         if(DEBUG) console.log(err);
         reject(err);
       } else {
